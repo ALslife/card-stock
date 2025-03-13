01291f6d045993c695e8cfc10c2cfc7cfbe101b5
ALslife <al20200816@gmail.com>
2025-03-13 23:30:14 +0900

    typeを親コンポーネントで管理できるように変更、パスワードのアイコンを配置

--- src/app/_components/input.tsx
+++ src/app/_components/input.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
+
 interface InputFormProps {
   label: string; // ラベル用のプロパティを定義
+  type: string;
 }
 
-const InputForm: React.FC<InputFormProps> = ({ label }) => {
+const InputForm: React.FC<InputFormProps> = ({ label, type }) => {
   return (
     <div className="relative w-full">
       <label className="absolute -top-2.5 left-1.5 px-2.5 bg-white text-[#757575]">
         {label} {/* プロパティからラベルを表示 */}
       </label>
-      <input type="text" className="border border-solid border-[#BDBDBD] w-full py-5 px-4 rounded-lg" />
+      <input type={type} className="border border-solid border-[#BDBDBD] w-full py-5 px-4 rounded-lg" />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,42 @@
+"use client"; // これを追加
+
 import InputForm from "./_components/input";
+import { useState } from "react";
 
 const Home: React.FC = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
   const inputFields = [
-    { label: "ユーザー名" },
-    { label: "メールアドレス" },
+    { label: "ユーザー名", type: "text" },
+    { label: "メールアドレス", type: "email" },
+    { label: "パスワード", type: "password" },
   ];
 
   return (
     <main className="pr-10 pl-10 mr-auto ml-auto w-full max-w-[750px]">
       {inputFields.map((field, index) => (
-        <div key={index} className={index < inputFields.length - 1 ? "mb-8" : ""}>
-          <InputForm label={field.label} />
+        <div
+          key={index}
+          className={
+            field.type === "password"
+              ? `relative ${index < inputFields.length - 1 ? "mb-8" : ""}`
+              : index < inputFields.length - 1
+              ? "mb-8"
+              : ""
+          }>
+          <InputForm
+            label={field.label}
+            type={
+              field.type === "password" && showPassword ? "text" : field.type
+            }
+          />
+          {field.type === "password" && (
+            <button onClick={togglePasswordVisibility} className="absolute right-5 top-5">
+              {showPassword ? <img src="/eye_close_fill.svg" /> : <img src="/eye_fill.svg" />}
+            </button>
+          )}
         </div>
       ))}
     </main>
