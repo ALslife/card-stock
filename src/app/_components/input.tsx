da0dc89dc13d6e3e8f66e548c123623159fb0798
ALslife <al20200816@gmail.com>
2025-03-14 02:09:10 +0900

    labelプロパティをオプショナルに変更、placeholderプロパティを追加

--- src/app/_components/input.tsx
+++ src/app/_components/input.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-
-
 interface InputFormProps {
-  label: string; // ラベル用のプロパティを定義
+  label?: string; // ラベル用のプロパティを定義
   type: string;
+  placeholder?: string;
 }
 
-const InputForm: React.FC<InputFormProps> = ({ label, type }) => {
+const InputForm: React.FC<InputFormProps> = ({ label, type, placeholder }) => {
   return (
     <div className="relative w-full">
-      <label className="absolute -top-2.5 left-1.5 px-2.5 bg-white text-[#757575]">
-        {label} {/* プロパティからラベルを表示 */}
-      </label>
-      <input type={type} className="border border-solid border-[#BDBDBD] w-full py-5 px-4 rounded-lg" />
+      {label && (
+        <label className="absolute -top-2.5 left-1.5 px-2.5 bg-white text-[#757575]">
+          {label} {/* プロパティからラベルを表示 */}
+        </label>
+      )}
+      <input type={type} className="border border-solid border-[#BDBDBD] w-full py-5 px-4 rounded-lg"
+      placeholder={placeholder || ''} 
+      />
     </div>
   );
 };
